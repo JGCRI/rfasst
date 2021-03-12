d1076d96055e5d7bd13c410254913ba8fe91c3e7
samp699 <jon.sampedro@pnnl.gov>
2021-03-12 15:52:28 -0500

    Add example function

--- R/ancillary_functions.R
+++ R/ancillary_functions.R
@@ -246,4 +246,25 @@ interpLinear <- function(d, y_start, y_end){
   return(d)
 }
 
+# exampleFunction
+#'
+#' Details: Add any details or general description about this function here.
+#' Details: Add any details or general description about this function here.
+#'
+#' @param param1 param1 Description
+#' @param param2 param2 Description
+#' @export
+#' @examples
+#' \donttest{
+#'     library(exampleRepo)
+#'     exampleFunction(3,4)
+#'     }
+
+exampleFunction<-function(param1=1,
+                          param2=2) {
+
+  return(param1+param2)
+
+}
+
 
